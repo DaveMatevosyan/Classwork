91545324603c929cc644a3bc9a970a1937c4562f
Adrine Kocharyan <adrine.kocharyan@optym.com>
2019-11-17 14:59:31 +0400

    Added Matrix class structure.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Matrix //: ArrayList
+    {
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+
+        private int[,] content;        
+
+        public int this[int i]
+        {
+            get => this.content[0, i];
+            //{
+            //    return this.content[0,i];
+            //}
+        }
+        public int this[int i, int j] //=> this.content[i,j];
+        {
+            get
+            {
+                return this.content[i, j];
+            }
+        }
+
+        public int this[int i, string j]
+        {
+            get
+            {
+                return 2;
+            }
+        }
+        public Matrix()
+        {
+            this.content = new int[this.Rows, this.Cols];
+        }
+
+        public Matrix(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            this.content = new int[Rows, Cols];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public Matrix Addition(Matrix a)
+        {
+            Matrix sum = new Matrix();
+            //...
+            return sum;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Sum of two matrices</returns>
+        public static Matrix Addition(Matrix a, Matrix b)
+        {
+            Matrix sum = new Matrix();
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Cols; j++)
+                {
+                    sum.content[i, j] = a[i, j] + b.content[i, j];
+                }
+            }
+            return sum;
+        }
+
+        public Matrix Multiplication(Matrix a, Matrix b)
+        {
+            Matrix mult = new Matrix();
+            //...
+            return mult;
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            Matrix sum = new Matrix();
+            //...
+            return sum;
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[,] x = new int[2,2];
+            //int[,] y = new int[2, 2];
+            //var sum = x + y;
+            Matrix aa;
+            Matrix a = new Matrix();
+            Matrix b = new Matrix();
+            Matrix c = new Matrix(3,3);
+            
+            int el = a[1, 1];
+
+            Matrix sum = a.Addition(b).Addition(c);
+            Matrix sum1 = Matrix.Addition(a, b);
+            Matrix sumOp = a + b;
+            Matrix sum3 = a + b + c;
+            a += b;
+        }
+    }
+}
