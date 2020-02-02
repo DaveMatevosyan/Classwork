df3023885e0088b6382a9031d976bb3c81e95d79
Adrine Kocharyan <adrine.kocharyan@optym.com>
2020-02-02 09:59:50 +0400

    Added image downloader project

---
+++ ImageDownloader/ImageDownloader/ImageDownloader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImageDownloader
+{
+    class ImageDownloaderClass
+    {
+        
+        public ImageDownloaderClass()
+        {
+           
+        }
+        public async Task<byte[]> DownloadImage(string path)
+        {
+            Console.WriteLine($"Start downloading an image from {path}...");
+            throw new NotImplementedException();
+        }
+
+        public async Task RotateImage(string imagePath)
+        {
+            Console.WriteLine($"Rotating {imagePath} image by 90 degrees...");
+
+            using (System.Drawing.Image img = System.Drawing.Image.FromFile(imagePath))
+            {
+                img.RotateFlip(RotateFlipType.Rotate90FlipX);
+                img.Save($"{imagePath.Split('.')[0]}Rotated.jpg");
+            }
+        }
+
+        public void SaveImage(byte[] bytes, string imagePath)
+        {
+            Console.WriteLine("Saving {0} image", imagePath);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ImageDownloader/ImageDownloader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] urls = new string[] { "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/C_Sharp_wordmark.svg/1200px-C_Sharp_wordmark.svg.png",
+                                           "https://www.vippng.com/png/detail/398-3984434_c-language-circle.png",
+                                           "https://i.pinimg.com/236x/97/cf/2c/97cf2ccd659ef9b00dd0aa15137130ec.jpg"};
+
+            ImageDownloaderClass imgDwn = new ImageDownloaderClass();
+
+            //TODO
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
